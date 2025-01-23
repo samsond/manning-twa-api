ea5a7e08a562cc0ef5f270dc2e0636b0e3ff7871
samsond <nosmaspoise@gmail.com>
2025-01-23 10:42:38 -0800

    feat: Add cache configuration and cache manager setup
    - Add CacheConfiguration class to manage cache properties (max entries, expiration duration).
    - Update application.yml and application-docker.yml to include cache configuration.
    - Create CacheManagerConfiguration class with a @Bean method named `cacheManager`.
    - Centralize cache configuration logic, including cache name and behavior for cache misses.

---
+++ api-provider-alpha/src/main/java/com/twa/flights/api/provider/alpha/configuration/CacheConfiguration.java
@@ -0,0 +1,28 @@
+package com.twa.flights.api.provider.alpha.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "cache")
+public class CacheConfiguration {
+
+    private int maximumSize;
+    private int expireAfterWriteMinutes;
+
+    public int getMaximumSize() {
+        return maximumSize;
+    }
+
+    public void setMaximumSize(int maximumSize) {
+        this.maximumSize = maximumSize;
+    }
+
+    public int getExpireAfterWriteMinutes() {
+        return expireAfterWriteMinutes;
+    }
+
+    public void setExpireAfterWriteMinutes(int expireAfterWriteMinutes) {
+        this.expireAfterWriteMinutes = expireAfterWriteMinutes;
+    }
+}
---
+++ api-provider-beta/src/main/java/com/twa/flights/api/provider/beta/configuration/CacheManagerConfiguration.java
@@ -0,0 +1,30 @@
+package com.twa.flights.api.provider.beta.configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheManagerConfiguration {
+
+    private final CacheConfiguration cacheConfiguration;
+
+    public CacheManagerConfiguration(CacheConfiguration cacheConfiguration) {
+        this.cacheConfiguration = cacheConfiguration;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder().maximumSize(cacheConfiguration.getMaximumSize())
+                .expireAfterWrite(cacheConfiguration.getExpireAfterWriteMinutes(), TimeUnit.MINUTES).recordStats());
+        return cacheManager;
+    }
+}
